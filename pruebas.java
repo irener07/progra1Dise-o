bdbefd4ac7ccd8017573cb3295e6673252a6a707
geromwatson <42458359+geromwatson@users.noreply.github.com>
2020-06-22 19:17:08 -0600

    pruebas

---
+++ pruebas.java
@@ -0,0 +1,49 @@
+package Algorithms;
+
+
+
+public class pruebas {
+
+	
+
+	public static void main(String[] args) {
+		
+		
+		//Prueba Cesar	
+				
+		//new Cesar("hola como estan",0,"cesar"); //lspd gsqs iwxdr
+		//new Cesar("krod frpr hvwdq",1,"cesar"); //hola 
+		
+		//Prueba Phone Codification
+		//new Phone_Code("password abc",0,"phone_code");
+		//new Phone_Code("71 21 74 74 91 63 73 31 *21 22 23",1,"phone_code");
+
+
+		//Prueba Binary Codification
+		//new Binary_Codification("sae que pa",0,"bin");
+		//new Binary_Codification("10010 00000 00100 *10000 10100 00100 *01111 00000",1,"bin");
+
+		//Prueba Reverse Message
+		//new Reverse_Message("codifiqueme esta",0,"reverse");	
+		//new Reverse_Message("atse emeuqifidoc",1,"reverse");	
+		
+		//Prueba Reverse Word
+		//new Reverse_Word("codifiqueme esta por favor",0,"reverse");	
+		//new Reverse_Word("atse emeuqifidoc",1,"reverse");
+		
+		//Prueba Key
+		new Key("codiqueme esta por favor mi saico usted sabe como es pa bombclat",0,"key","tango");
+		new Key("voqoenezk xsgg ioe yaiuf fi lavic nsgkr laok vozu xs ia uozhqeag",1,"key","tango");
+
+		
+		
+		
+
+		
+
+
+				
+
+	}
+
+}
