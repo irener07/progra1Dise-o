22b6c0bfb8d793be1e7f5531775663edd7f4f404
geromwatson <42458359+geromwatson@users.noreply.github.com>
2020-06-25 01:26:39 -0600

    pruebas de todos los algoritmos

--- pruebas.java
+++ pruebas.java
@@ -18,7 +18,6 @@ public class pruebas {
 		//new Phone_Code("password abc",0,"phone_code");
 		//new Phone_Code("71 21 74 74 91 63 73 31 *21 22 23",1,"phone_code");
 
-
 		//Prueba Binary Codification
 		//new Binary_Codification("sae que pa",0,"bin");
 		//new Binary_Codification("10010 00000 00100 *10000 10100 00100 *01111 00000",1,"bin");
@@ -28,22 +27,18 @@ public class pruebas {
 		//new Reverse_Message("atse emeuqifidoc",1,"reverse");	
 		
 		//Prueba Reverse Word
-		//new Reverse_Word("codifiqueme esta por favor",0,"reverse");	
-		//new Reverse_Word("atse emeuqifidoc",1,"reverse");
-		
-		//Prueba Key
-		new Key("codiqueme esta por favor mi saico usted sabe como es pa bombclat",0,"key","tango");
-		new Key("voqoenezk xsgg ioe yaiuf fi lavic nsgkr laok vozu xs ia uozhqeag",1,"key","tango");
-
-		
-		
-		
-
-		
-
-
+		//new Reverse_Word("codifiqueme esta por favor mi compita usted sabe como es",0,"reverse");	
+		//new Reverse_Word("emeuqifidoc atse rop rovaf im atipmoc detsu ebas omoc se",1,"reverse");	
 				
+		//Prueba Key
+		//new Key("codiqueme esta por favor mi saico usted sabe como es pa bombclat",0,"key","tango");
+		//new Key("voqoenezk xsgg ioe yaiuf fi lavic nsgkr laok vozu xs ia uozhqeag",1,"key","tango");
 
+		//Prueba Vigenere
+		//new Vigenere("tarea zzz programada criptografia de datos",0,"vigenere",23);
+		//new Vigenere("vdthc bcb ruqjtdodfd euksvriucikd fh fdvru",1,"vigenere",23);
+
+		
 	}
 
 }
